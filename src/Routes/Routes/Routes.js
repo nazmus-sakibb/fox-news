34c723f8c625145e4021a12bd1f3d0eddd7e3ff9
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-10 23:59:49 +0600

    router setup

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,15 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../../layout/Main';
+// import Home from '../../components/Home/Home';
 
 export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main />,
-        children: [
-            {
-                path: '/',
-                element: 
-            }
-        ]
+        // children: [
+        //     {
+        //         path: '/',
+        //         element: <Home/>
+        //     }
+        // ]
     }
 ]);
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Main = () => {
     return (
         <div>
-            <Outlet/>
+            <Container>
+                <Row>
+                    <Col lg='2'>
+                        <h2>SideNav</h2>
+                    </Col>
+                    <Col lg='5'>
+                        <Outlet />
+                    </Col>
+                    <Col lg='3'>
+                        <h2>Right Side Nav</h2>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
