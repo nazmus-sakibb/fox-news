5d885bd6c66b6b221a5ad57fef5144e3d79f856e
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-10 22:17:56 +0600

    crate main component

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,15 @@
-import {createBrowserRouter} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../layout/Main';
 
 export const routes = createBrowserRouter([
-    
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: 
+            }
+        ]
+    }
 ]);
