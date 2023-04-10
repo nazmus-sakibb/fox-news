5f0102920c7b3e3d90025866db48170cb987b140
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-11 00:10:57 +0600

    folder structure

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../../layout/Main';
-// import Home from '../../components/Home/Home';
 
 export const routes = createBrowserRouter([
     {
