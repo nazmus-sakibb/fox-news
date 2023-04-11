e260774f49e33d1ed0f72e6e8f07086e6e208ed6
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-12 01:03:46 +0600

    navbar complete

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -3,7 +3,7 @@ import { ListGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { FaFacebook, FaGithub, FaGoogle, FaLinkedin, FaTwitch, FaTwitter, FaWhatsapp } from 'react-icons/fa';
-import Carousel from 'react-bootstrap/Carousel';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
 
 const RightSideNav = () => {
     return (
@@ -13,7 +13,7 @@ const RightSideNav = () => {
                 <Button className='mb-5' variant='outline-dark'><FaGithub /> Login with GitHub</Button>
             </ButtonGroup>
 
-            <div>
+            <div className='mb-5'>
                 <h4>Find Us On</h4>
                 <ListGroup>
                     <ListGroup.Item className='mb-2'><FaFacebook /> Facebook</ListGroup.Item>
@@ -26,45 +26,7 @@ const RightSideNav = () => {
 
             {/* carousel */}
             <div>
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=First slide&bg=373940"
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=Second slide&bg=282c34"
-                            alt="Second slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=Third slide&bg=20232a"
-                            alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>
-                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                            </p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+                <BrandCarousel />
             </div>
         </div>
     );
