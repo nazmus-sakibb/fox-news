b5174ddc000f180c3e6e4b3785d7f65a9e0bc545
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-11 12:35:25 +0600

    setup firebase, react-router-dom, react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { RouterProvider } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import { routes } from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider routes={routes}></RouterProvider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
