3ec58cde91cfc5b9ba1b7f39dcf99b0020652a14
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-15 15:53:06 +0600

    style autor info on the news card

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image'
+import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
 
 const NewsSummaryCard = ({ news }) => {
     const { _id, title, author, details, image_url, total_view } = news;
     console.log(news);
     return (
-        <Card className="">
-            <Card.Header>Featured</Card.Header>
+        <Card className="mb-5">
+            <Card.Header className='d-flex justify-content-between align-items-center'>
+                <div className='d-flex justify-content-between align-items-center'>
+                    <Image className='me-3' src={author.img} style={{ height: '50px' }} roundedCircle />
+                    <div>
+                        <p className='m-0'>{author.name}</p>
+                        <p className='m-0'>P{author.published_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <FaRegBookmark />
+                    <FaShareAlt />
+                </div>
+            </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
                 <Card.Text>
                     {details.length > 250 ?
                         <p>{details.slice(0, 250) + '...'} <Link to={`/news/${_id}`}>Read More</Link></p>
