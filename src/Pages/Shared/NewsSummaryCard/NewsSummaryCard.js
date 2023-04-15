bbb4b3bc9e3269734adac017295d0cb26e7b1f9b
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-15 15:35:14 +0600

    set category news data on the news card

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
-const NewsSummaryCard = ({news}) => {
+const NewsSummaryCard = ({ news }) => {
+    const { _id, title, author, details, image_url, total_view } = news;
+    console.log(news);
     return (
-        <div>
-            {news.title}
-        </div>
+        <Card className="">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {details.length > 250 ?
+                        <p>{details.slice(0, 250) + '...'} <Link to={`/news/${_id}`}>Read More</Link></p>
+                        :
+                        <p>{details}</p>
+                    }
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        </Card>
     );
 };
 
