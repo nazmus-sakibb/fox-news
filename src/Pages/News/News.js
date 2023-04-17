bcee091fce63f1a406f6888eeafe3c141f89586e
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-17 21:28:15 +0600

    show news details dynamically

--- src/Pages/News/News.js
+++ src/Pages/News/News.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import Category from '../Category/Category';
 
 const News = () => {
     const news = useLoaderData();
+    console.log(news);
+    const { title, details, category_id, author, image_url, rating } = news;
 
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+        <Card>
+            <Card.Img variant="top" src={image_url} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{title}</Card.Title>
                 <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    {details}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Link to={`/category/${category_id}`}>
+                    <Button variant="primary">All News in this Category</Button>
+                </Link>
             </Card.Body>
         </Card>
     );
