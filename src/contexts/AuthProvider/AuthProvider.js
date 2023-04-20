73814c55af18f0833adbb8fec155fdfbabcaae5a
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-21 01:15:59 +0600

    set google sign in popup auth

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,11 +1,17 @@
 import React, { createContext } from 'react';
-
+import { getAuth, signInWithPopup } from 'firebase/auth';
 export const AuthContext = createContext();
+
+const auth = getAuth();
 
 const AuthProvider = ({ children }) => {
     const user = { displayName: 'Batash ali' };
 
-    const authInfo = { user };
+    const providerLogin = (provider) => {
+        return signInWithPopup(auth, provider);
+    }
+
+    const authInfo = { user, providerLogin };
 
     return (
         <AuthContext.Provider value={authInfo}>
