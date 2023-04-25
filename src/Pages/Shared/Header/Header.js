c48c906b57edd140422940d44d5a064913f8a070
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-25 22:54:35 +0600

    implement google sign in and display user info

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -6,6 +6,8 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import LeftSideNav from '../LeftSideNav/LeftSideNav';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import { Image } from 'react-bootstrap';
+import { FaUser } from 'react-icons/fa';
 
 const Header = () => {
     const { user } = useContext(AuthContext);
@@ -34,7 +36,15 @@ const Header = () => {
                     <Nav>
                         <Nav.Link href="#deets">{user?.displayName}</Nav.Link>
                         <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
+                            {user?.photoURL ?
+                                <Image
+                                    style={{ height: '30px' }}
+                                    roundedCircle
+                                    src={user.photoURL}
+                                />
+                                :
+                                <FaUser />
+                            }
                         </Nav.Link>
                     </Nav>
                     <div className='d-lg-none'>
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,15 +1,27 @@
-import React, { createContext } from 'react';
-import { getAuth, signInWithPopup } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import app from '../../firebase/firebase.config';
+
 export const AuthContext = createContext();
 
-const auth = getAuth();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const user = { displayName: 'Batash ali' };
+    const [user, setUser] = useState(null);
 
     const providerLogin = (provider) => {
         return signInWithPopup(auth, provider);
     }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('Inside auth state changed', currentUser);
+            setUser(currentUser);
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, []);
 
     const authInfo = { user, providerLogin };
 
