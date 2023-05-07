66e17c66e7f426dc7743656e1d753f6d0955285b
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-07 12:30:19 +0600

    create resgitration form

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register</h2>
-        </div>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Your Name</Form.Label>
+                <Form.Control name='name' type="text" placeholder="Your Name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Photo URL</Form.Label>
+                <Form.Control name="photoURL" type="text" placeholder="Photo URL" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control name="email" type="email" placeholder="Enter email" required/>
+            </Form.Group>
+            
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name="password" type="password" placeholder="Password" required/>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Register
+            </Button>
+            <Form.Text className="text-danger">
+                {/* We'll never share your email with anyone else. */}
+            </Form.Text>
+        </Form>
     );
 };
 
